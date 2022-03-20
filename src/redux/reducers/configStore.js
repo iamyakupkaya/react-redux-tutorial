addfe5ae7e10ead9b77e819ed4115492f86c2fb5
Yakup KAYA <iamyakupkaya@outlook.com>
2022-03-20 02:42:45 +0200

    Created a createStore method to see clearly how createStore method works

--- src/redux/reducers/configStore.js
+++ src/redux/reducers/configStore.js
@@ -6,3 +6,34 @@ const configStore = () => {
 }
 
 export default configStore;
+
+// how does createStore work? let's explain it... and create our createStore method
+/*
+
+const createStore = (reducer) => {
+
+    let state;
+    //subscribe can be invoked too much so let's describe a listeners list
+    let listeners = [];
+    const getState = () => state;
+
+    const dispatch = () => {
+        state = reducer(state,action) // calculate new state
+        listeners.forEach(listener => listener()) // all lisneter func. will be invoked
+    }
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+
+        // below code for unlisten
+        return () => {
+            listeners = listeners.filter(element => element !== listener)
+        }
+    }
+
+    dispatch({}) // for initial getState
+    
+    return {getState, dispatch, subscribe};
+}
+
+*/
