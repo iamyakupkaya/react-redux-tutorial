6ee711aeae9ee9e1a666c8ae80e5488477a4738a
Yakup KAYA <iamyakupkaya@outlook.com>
2022-03-20 03:11:06 +0200

    Added a parametre to dispatch method for 'Created a createStore method to see clearly how createStore method works' commit

--- src/redux/reducers/configStore.js
+++ src/redux/reducers/configStore.js
@@ -1,8 +1,39 @@
-import {createStore} from 'redux';
-import reducers from './index';
+import { createStore } from "redux";
+import reducers from "./index";
 
 const configStore = () => {
-    return createStore (reducers);
-}
+  return createStore(reducers);
+};
 
 export default configStore;
+
+// how does createStore work? let's explain it... and create our createStore method
+/*
+
+const createStore = (reducer) => {
+
+    let state;
+    //subscribe can be invoked too much so let's describe a listeners list
+    let listeners = [];
+    const getState = () => state;
+
+    const dispatch = (action) => {
+        state = reducer(state,action) // calculate new state
+        listeners.forEach(listener => listener()) // all lisneter func. will be invoked
+    }
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+
+        // below code for unlisten
+        return () => {
+            listeners = listeners.filter(element => element !== listener)
+        }
+    }
+
+    dispatch({}) // for initial getState
+    
+    return {getState, dispatch, subscribe};
+}
+
+*/
